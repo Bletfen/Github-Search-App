22d187ee6a17c3ed06724882d25024e472f0088f
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 23:16:26 +0400

    MainDisplay

--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -1,4 +1,9 @@
 import Logo from "/assets/icon-moon.svg";
+import Location from "/assets/icon-location.svg";
+import WebSite from "/assets/icon-website.svg";
+import Twitter from "/assets/icon-twitter.svg";
+import Company from "/assets/icon-company.svg";
+
 export default function MainDisplay() {
   return (
     <div>
@@ -24,6 +29,28 @@ export default function MainDisplay() {
         <div>
           <span>Following</span>
           <span>9</span>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <img src={Location} alt="icon-location" />
+            <span>San Francisco</span>
+          </div>
+          <div>
+            <img src={WebSite} alt="icon-website" />
+            <span>https://github.blog</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src={Twitter} alt="icon-twitter" />
+            <span>Not Available</span>
+          </div>
+          <div>
+            <img src={Company} alt="icon-company" />
+            <span>@github</span>
+          </div>
         </div>
       </div>
     </div>
