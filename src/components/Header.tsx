a2ff604eecf8c619d48ab6bd9a1348e105d3176a
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 22:57:23 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,12 @@ export default function Header() {
   return (
     <div
       className="flex justify-between 
-      px-[2.4rem] pt-[3.1rem] items-center"
+      px-[2.4rem] pt-[3.1rem] items-center
+      text-[#fff]"
     >
-      <h1>devfinder</h1>
+      <h1 className="text-[2.6rem] font-[700]">devfinder</h1>
       <div className="flex items-center gap-[1.6rem]">
-        <p>LIGHT</p>
+        <p className="text-[1.3rem] font-[700] tracking-[0.25rem]">LIGHT</p>
         <img src={SunSvg} alt="sun-icon" />
       </div>
     </div>
