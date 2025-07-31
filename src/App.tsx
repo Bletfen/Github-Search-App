98a37514bbf613f95788307f36a7f9016f476ecf
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 22:53:20 +0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css";
 import Header from "./components/Header";
 function App() {
   return (
-    <>
+    <div className="bg-[#141d2f] h-screen">
       <Header />
-    </>
+    </div>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,10 @@ export default function Header() {
   return (
     <div>
       <h1>devfinder</h1>
+      <div>
+        <p>LIGHT</p>
+        <img src={SunSvg} alt="sun-icon" />
+      </div>
     </div>
   );
 }
