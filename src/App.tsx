dc925a0d726848c2911d95e204c3c9dfabd2c8a9
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 23:11:21 +0400

    MainDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import SearchInput from "./components/SearchInput";
+import MainDisplay from "./components/MainDisplay";
 function App() {
   return (
     <div className="bg-[#141d2f] h-screen px-[2.4rem]">
       <Header />
       <SearchInput />
+      <MainDisplay />
     </div>
   );
 }
--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -1,3 +1,10 @@
+import Logo from "/assets/icon-moon.svg";
 export default function MainDisplay() {
-  return <div>MainDisplay</div>;
+  return (
+    <div>
+      <div>
+        <img src={Logo} alt="" />
+      </div>
+    </div>
+  );
 }
