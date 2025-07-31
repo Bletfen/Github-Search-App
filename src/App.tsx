1234fe7a0f73edc5cfdfa2424af085dfffa356b3
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 23:32:46 +0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import SearchInput from "./components/SearchInput";
 import MainDisplay from "./components/MainDisplay";
 function App() {
   return (
-    <div className="bg-[#141d2f] h-screen px-[2.4rem]">
+    <div
+      className="bg-[#141d2f] h-screen px-[2.4rem] flex
+    flex-col items-center justify-center"
+    >
       <Header />
       <SearchInput />
       <MainDisplay />
