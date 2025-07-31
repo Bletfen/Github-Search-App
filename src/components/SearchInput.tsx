e40be52901d4a923dfd78f3488d59120aaba5638
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 23:07:38 +0400

    SearchInput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,7 +3,7 @@ export default function SearchInput() {
   return (
     <div
       className="px-[2.4rem] text-[#fff] w-full
-      flex py-[2rem] pl-[1.6rem] pr-[0.7rem]
+      flex pt-[0.65rem] pb-[0.75rem] pl-[1.6rem] pr-[0.7rem]
       rounded-[1.5rem] bg-[#1e2a47] items-center"
     >
       <img src={SearchSvg} alt="search-icon" className="mr-[0.8rem]" />
@@ -13,7 +13,13 @@ export default function SearchInput() {
         className="mr-[0.7rem] w-full
         text-[#fff] text-[1.3rem] font-[400] leading-[2.5rem]"
       />
-      <button>Search</button>
+      <button
+        className="py-[1.2rem] px-[1.6rem]
+      rounded-[1rem] bg-[#0079ff] text-[1.4rem] font-[700]
+      cursor-[pointer]"
+      >
+        Search
+      </button>
     </div>
   );
 }
