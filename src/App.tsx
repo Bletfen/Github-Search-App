3a2177b7059948f237ec43f013fc2e275f773af5
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 22:58:59 +0400

    SearchInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
+import SearchInput from "./components/SearchInput";
 function App() {
   return (
     <div className="bg-[#141d2f] h-screen">
       <Header />
+      <SearchInput />
     </div>
   );
 }
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,3 +1,9 @@
+import SearchSvg from "/assets/search-icon.svg";
 export default function SearchInput() {
-  return <div>SearchInput</div>;
+  return (
+    <div>
+      <img src={SearchSvg} alt="search-icon" />
+      <input type="text" />
+    </div>
+  );
 }
