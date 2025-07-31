8c7794f5c75a324ab3d101434ef3c35111e4c74c
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 22:53:41 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import SunSvg from "/assets/icon-sun.svg";
 export default function Header() {
   return (
-    <div>
+    <div className="flex">
       <h1>devfinder</h1>
-      <div>
+      <div className="flex">
         <p>LIGHT</p>
         <img src={SunSvg} alt="sun-icon" />
       </div>
