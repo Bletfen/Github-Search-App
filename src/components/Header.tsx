aa4c29583c2c455fa8116e774056c21392dd3a27
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-31 23:37:10 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
       <h1 className="text-[2.6rem] font-[700]">devfinder</h1>
       <div className="flex items-center gap-[1.6rem]">
         <p className="text-[1.3rem] font-[700] tracking-[0.25rem]">LIGHT</p>
-        <img src={SunSvg} alt="sun-icon" />
+        <img src={SunSvg} alt="sun-icon" className="cursor-[pointer]" />
       </div>
     </div>
   );
